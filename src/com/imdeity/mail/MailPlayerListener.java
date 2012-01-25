20dbc3a7f89f1971a1d84e3dce193c759f064e18
Ryan vanZeben <vanZeben@live.ca>
2012-01-25 07:40:10 -0500

    added mail.join perm node

--- src/com/imdeity/mail/MailPlayerListener.java
+++ src/com/imdeity/mail/MailPlayerListener.java
@@ -6,24 +6,32 @@ import org.bukkit.event.player.PlayerListener;
 
 public class MailPlayerListener extends PlayerListener {
 
-    public MailPlayerListener(Mail instance) {
+	public MailPlayerListener(Mail instance) {
 
-    }
+	}
 
-    @Override
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        int unread = MailSQL.getUnreadCount(player.getName());
-        if (unread > 0)
-            if (unread == 1)
-                ChatTools.formatAndSend(("<option><white>You have <yellow>"
-                        + unread + "<white> unread message in your inbox."), "Mail", player);
-            else
-                ChatTools.formatAndSend(("<option><white>You have <yellow>"
-                        + unread + "<white> unread messages in your inbox."), "Mail", player);
-        else
-            ChatTools.formatAndSend(
-                    ("<option><gray>Nothing in your inbox."), "Mail",
-                    player);
-    }
+	@Override
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		if (Mail.permissions.has(player, "mail.join")) {
+			int unread = MailSQL.getUnreadCount(player.getName());
+			if (unread > 0)
+				if (unread == 1)
+					ChatTools
+							.formatAndSend(
+									("<option><white>You have <yellow>"
+											+ unread + "<white> unread message in your inbox."),
+									"Mail", player);
+				else
+					ChatTools
+							.formatAndSend(
+									("<option><white>You have <yellow>"
+											+ unread + "<white> unread messages in your inbox."),
+									"Mail", player);
+			else
+				ChatTools.formatAndSend(
+						("<option><gray>Nothing in your inbox."), "Mail",
+						player);
+		}
+	}
 }
