5b7abeb690d54d19a91a865a4ef93aa067bd0d51
vanZeben <vanZeben@live.ca>
2012-08-02 04:36:09 -0400

    Missing file commited

---
+++ src/com/imdeity/mail/MailConfigHelper.java
@@ -0,0 +1,9 @@
+package com.imdeity.mail;
+
+public class MailConfigHelper {
+    
+    public static final String MAIL_COST_INBOX = "mail.costs.inbox";
+    public static final String MAIL_COST_READ = "mail.costs.read";
+    public static final String MAIL_COST_WRITE = "mail.costs.write";
+    public static final String MAIL_CLOSE_MATCH = "mail.closeness_match";
+}
